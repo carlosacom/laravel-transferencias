24a3fc17f2d09733b08a80f2d94515312f13c6cb
Carlos Andres Perez Combariza <carlosaperez1997@gmail.com>
2019-04-29 21:51:12 -0500

    primer commit creado algunos modelos y migraciones

---
+++ database/migrations/2019_04_30_015408_create_resellers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResellersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resellers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('document')->unique();
+            $table->string('phone')->nullable(null);
+            $table->string('whatsapp')->nullable(null);
+            $table->string('token')->unique();
+            $table->string('minimum_value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resellers');
+    }
+}
