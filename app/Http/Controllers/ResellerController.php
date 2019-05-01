8b6076a8555ad941a190f145a316978b095f02b4
Carlos Andres Perez Combariza <carlosaperez1997@gmail.com>
2019-04-30 22:19:40 -0500

    Controlador de reseller CRUD para el master

--- app/Http/Controllers/ResellerController.php
+++ app/Http/Controllers/ResellerController.php
@@ -42,8 +42,105 @@ class ResellerController extends Controller
         try {
             $dataRequest = array_map('trim', $request->all());
             $validate = \Validator::make($dataRequest, array(
-                ''
+                'name' => 'required|string|min:2|max:175',
+                'email' => 'required|email|max:175|unique:resellers,email',
+                'document' => 'required|string|min:5|max:20|unique:resellers,document',
+                'phone' => 'nullable|string|min:5|max:25',
+                'whatsapp' => 'nullable|string|min:5|max:25',
+                'minimum_value' => 'required|numeric',
+                'password' => 'required|string|min:8|max:21|confirmed'
             ));
+            if (!$validate->fails()) {
+                $reseller = new Reseller();
+                $reseller->name = $dataRequest['name'];
+                $reseller->email = $dataRequest['email'];
+                $reseller->document = $dataRequest['document'];
+                $reseller->phone = isset($dataRequest['phone']) ? $dataRequest['phone'] : null ;
+                $reseller->whatsapp = isset($dataRequest['whatsapp']) ? $dataRequest['whatsapp'] : null ;
+                $reseller->minimum_value = $dataRequest['minimum_value'];
+                $reseller->password = \Hash::make($dataRequest['password']);
+                $reseller->save();
+                $response = array(
+                    'status' => 200,
+                    'response' => $reseller
+                );
+            } else {
+                $response = array(
+                    'status' => 400,
+                    'response' => array('errors' => $validate->errors())
+                );
+            }
+        } catch (\Exception $e) {
+            $response = array(
+                'status' => 500,
+                'response' => array('errors' => 'Error en el servidor ' . $e->getMessage())
+            );
+        }
+        return response()->json($response['response'],$response['status']);
+    }
+    public function update(String $id, Request $request)
+    {
+        try {
+            $dataRequest = array_map('trim', $request->all());
+            $validate = \Validator::make($dataRequest, array(
+                'name' => 'required|string|min:2|max:175',
+                'email' => 'required|email|max:175|unique:resellers,email,' . $id,
+                'document' => 'required|string|min:5|max:20|unique:resellers,document, '. $id,
+                'phone' => 'nullable|string|min:5|max:25',
+                'whatsapp' => 'nullable|string|min:5|max:25',
+                'minimum_value' => 'required|numeric'
+            ));
+            if (!$validate->fails()) {
+                $reseller = Reseller::find($id);
+                if ($reseller) {
+                    $reseller->name = $dataRequest['name'];
+                    $reseller->email = $dataRequest['email'];
+                    $reseller->document = $dataRequest['document'];
+                    $reseller->phone = isset($dataRequest['phone']) ? $dataRequest['phone'] : null ;
+                    $reseller->whatsapp = isset($dataRequest['whatsapp']) ? $dataRequest['whatsapp'] : null ;
+                    $reseller->minimum_value = $dataRequest['minimum_value'];
+                    $reseller->update();
+                    $response = array(
+                        'status' => 200,
+                        'response' => $reseller
+                    );
+                } else {
+                    $response = array(
+                        'status' => 404,
+                        'response' => array('errors' => 'No existe el reseller')
+                    );
+                }
+            } else {
+                $response = array(
+                    'status' => 400,
+                    'response' => array('errors' => $validate->errors())
+                );
+            }
+        } catch (\Exception $e) {
+            $response = array(
+                'status' => 500,
+                'response' => array('errors' => 'Error en el servidor ' . $e->getMessage())
+            );
+        }
+        return response()->json($response['response'],$response['status']);
+    }
+    public function destroy(String $id)
+    {
+        try {
+            $reseller = Reseller::find($id);
+            if ($reseller) {
+                $reseller->enable = false;
+                $reseller->update();
+                $response = array(
+                    'status' => 200,
+                    'response' => $reseller
+                );
+            } else {
+                $response = array(
+                    'status' => 404,
+                    'response' => array('errors' => 'No existe el reseller')
+                );
+            }
         } catch (\Exception $e) {
             $response = array(
                 'status' => 500,
